d6375898e6ab8820bbcde54f850343ef15e25178
Vadym Holovin <holyvad@gmail.com>
2017-12-05 19:11:02 +0300

    Disabled slider controls in 1st and last positions

--- src/js/common.js
+++ src/js/common.js
@@ -3,14 +3,14 @@
    * BRIEF SLIDER
    */
   var slideIndex = 1;
-  var prev = document.querySelector(".slider-controls--prev");
-  var next = document.querySelector(".slider-controls--next");
+  var firstSlide = document.querySelector("#btn-prev");
+  var lastSlide = document.querySelector("#slide-6 #btn-next");
+  var prev = document.querySelector("#btn-prev");
+  var next = document.querySelector("#btn-next");
   showDivs(slideIndex);
 
   prev.addEventListener("click", function(e) {
     plusDivs(-1);
-
-
   });
 
   next.addEventListener("click", function(e) {
@@ -23,12 +23,14 @@
 
   function showDivs(n) {
     var x = document.querySelectorAll(".brief-slide");
-    var submit = document.querySelector("#submit");
 
     if (n > x.length) {slideIndex = 1}
     if (n < 1) {slideIndex = x.length}
     for (var i = 0; i < x.length; i++) {
-        x[i].classList.remove("active"); 
+        x[i].classList.remove("active");
+
+        (n == 1) ? prev.setAttribute("disabled", "") : prev.removeAttribute("disabled", "");
+        (n == x.length) ? next.setAttribute("disabled", "") : next.removeAttribute("disabled", "");
     }
     x[slideIndex-1].classList.add("active");
   }
@@ -38,11 +40,13 @@
 
 $(document).ready(function() {
   
+  /* SCROLL TO ID INITIALIZATION */
+
   $(".slider-controls--prev, .slider-controls--next").mPageScroll2id();
 
 });
 
-/*  */
+
 
 $(document).ready(function() {
   var $rangeslider = $('#js-amount-range');
@@ -60,3 +64,4 @@ $(document).ready(function() {
     $rangeslider.val(this.value).change();
   });
 });
+
